6641e097b5b07138254703ced7fad434aad96d54
asmaa <elsaeeedasmaa@gmail.com>
2025-10-30 15:51:00 +0300

    adding hydrated cubit theme done el done 💪💃✅

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:testing_in_flutter/app.dart';
+import 'package:path_provider/path_provider.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  HydratedBloc.storage = await HydratedStorage.build(
+    storageDirectory: kIsWeb
+        ? HydratedStorageDirectory.web
+        : HydratedStorageDirectory((await getTemporaryDirectory()).path),
+  );
   runApp(const AppThemeing());
 }
