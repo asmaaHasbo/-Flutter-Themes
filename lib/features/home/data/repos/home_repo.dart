7dbcca5730b8dd0052585c555db4b22bdbe73d6e
asmaa <elsaeeedasmaa@gmail.com>
2025-11-05 17:48:34 +0200

    adding hive logic in home repo class done el done 💃✅

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -5,21 +5,36 @@ import 'package:testing_in_flutter/core/error/error_handler.dart';
 import 'package:testing_in_flutter/core/error/error_model.dart';
 import 'package:testing_in_flutter/core/networking/api_result.dart';
 import 'package:testing_in_flutter/features/home/data/api_service/home_api_service.dart';
+import 'package:testing_in_flutter/features/home/data/local_data/home_local_data.dart';
 import 'package:testing_in_flutter/features/home/data/model/movie_model.dart';
 
 class HomeRepo {
   HomeApiService homeApiService;
   HomeRepo(this.homeApiService);
 
+  HomeLocalData homeLocalData = HomeLocalData();
+
   Future<ApiResult<List<MovieModel>>> getPopularMovies() async {
+    final storedMovies = homeLocalData.getCachedPopularMovies();
+
     try {
       final response = await homeApiService.getPopularMovies();
+
+      homeLocalData.storePopularMovies(response.results);
+
       return ApiResult.success(response.results);
     } on DioException catch (e) {
       log('Dio error ${e.toString()}');
 
+      if (storedMovies.isNotEmpty) {
+        homeLocalData.storePopularMovies(storedMovies);
+      }
+
       return ApiResult.failure(handleDioException(e));
     } catch (e) {
+      if (storedMovies.isNotEmpty) {
+        homeLocalData.storePopularMovies(storedMovies);
+      }
       log('catch error ${e.toString()}');
       return ApiResult.failure(ErrorModel(message: e.toString()));
     }
